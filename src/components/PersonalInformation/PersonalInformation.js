1e3233ab90df2c933e035790a1edb1011043f9e9
lucaleivaloop <lleiva@loopstud.io>
2020-01-01 20:45:31 -0300

    Add image and improve a11y

--- src/components/PersonalInformation/PersonalInformation.js
+++ src/components/PersonalInformation/PersonalInformation.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './PersonalInformation.module.css';
+import photo from './portrait.png';
 
 const PersonalInformation = () => (
   <section>
     <article>
       <h1 className={styles.name}>Luca Leiva</h1>
+      <img className={styles.photo} src={photo} alt="" />
       <h2 className={styles.location}>Montevideo, Uruguay</h2>
       <p>
         Curious and detail-oriented full-stack developer who strives to deliver
@@ -19,21 +21,24 @@ const PersonalInformation = () => (
         rel="noopener noreferrer"
         href="https://www.linkedin.com/in/luca-leiva-gonzalez-9002a9130/"
       >
-        <FontAwesomeIcon icon={['fab', 'linkedin']} />
+        <FontAwesomeIcon icon={['fab', 'linkedin']} aria-hidden="true" />
+        <span className={styles.hidden}>LinkedIn Profile</span>
       </a>
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://github.com/lucaleivaloop"
       >
-        <FontAwesomeIcon icon={['fab', 'github']} />
+        <FontAwesomeIcon icon={['fab', 'github']} aria-hidden="true" />
+        <span className={styles.hidden}>GitHub Profile</span>
       </a>
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://github.com/eleleiva248/resume"
       >
-        <FontAwesomeIcon icon={'code'} />
+        <FontAwesomeIcon icon={'code'} aria-hidden="true" />
+        <span className={styles.hidden}>GitHub Repository</span>
       </a>
     </article>
   </section>
