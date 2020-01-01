fa642b2dae3aaa185d29d15d0144534cc42b01a8
Luca Leiva <39443365+eleleiva248@users.noreply.github.com>
2020-01-01 13:41:06 -0300

    Add icons and links (#3)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 import MainPage from './pages/MainPage';
+
+library.add(fab, fas);
 
 function App() {
   return <MainPage />;
