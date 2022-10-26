828643daa62b3789bfe8cd3fe1419dab0556b7fe
Luca Leiva <lucaleiva248@gmail.com>
2022-10-26 17:45:12 -0300

    Update dependencies

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import './app.css';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
