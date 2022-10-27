727f24941e0957de90d899e88742f1ad0add66ab
Luca Leiva <32399007+eleleiva@users.noreply.github.com>
2022-10-27 13:56:36 -0300

    Remove umami analytics (#305)

--- src/components/Inputs/Input.js
+++ src/components/Inputs/Input.js
@@ -2,11 +2,11 @@ import { Label } from './Label';
 
 import styles from './Input.module.css';
 
-function Input({ eventClass, errors, label, name, placeholder, register }) {
+function Input({ errors, label, name, placeholder, register }) {
   return (
     <Label errors={errors} label={label} name={name}>
       <input
-        className={`${styles.input} ${eventClass ?? ''}`}
+        className={styles.input}
         id={name}
         placeholder={placeholder}
         type="text"
--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,33 +1,26 @@
 import Head from 'next/head';
-import Script from 'next/script';
 
 import picture from 'assets/images/ll.webp';
 
 function SEO() {
   return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/l.png" />
-        <meta name="theme-color" content="#eee" />
-        <meta name="description" content="Luca Leiva" />
-        <link rel="apple-touch-icon" href="/l.png" />
+    <Head>
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/l.png" />
+      <meta name="theme-color" content="#eee" />
+      <meta name="description" content="Luca Leiva" />
+      <link rel="apple-touch-icon" href="/l.png" />
 
-        <link rel="manifest" href="/manifest.json" />
+      <link rel="manifest" href="/manifest.json" />
 
-        <title>Luca Leiva</title>
-        <meta property="og:title" content="Luca Leiva" />
-        <meta
-          property="og:description"
-          content="Luca Leiva is a a web developer whose main focus is front-end development. He always try his best to improve users as well as developers' experience in every project he lays hands on."
-        />
-        <meta property="og:image" content={picture.src} />
-      </Head>
-      <Script
-        data-website-id={process.env.NEXT_PUBLIC_ANALYTICS_KEY}
-        src={process.env.NEXT_PUBLIC_ANALYTICS_URL}
+      <title>Luca Leiva</title>
+      <meta property="og:title" content="Luca Leiva" />
+      <meta
+        property="og:description"
+        content="Luca Leiva is a a web developer whose main focus is front-end development. He always try his best to improve users as well as developers' experience in every project he lays hands on."
       />
-    </>
+      <meta property="og:image" content={picture.src} />
+    </Head>
   );
 }
 
