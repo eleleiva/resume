d6fa7b0e9f6cff777daa8f1c19fd2622fd95a659
Luca Leiva <32399007+eleleiva@users.noreply.github.com>
2024-02-24 11:02:20 -0300

    Feature/update dependencies 24 02 24 (#334)
    * Update dependencies
    
    * Update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter()
 	}
